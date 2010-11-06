8ddb08dc7969b3254c61a08d5ee989baaa71df2b
Nicolas Viennot <nicolas@viennot.biz>
2010-11-05 20:32:33 -0400

    Open: rewrote do_sys_open() in a more linear code style

--- fs/open.c
+++ fs/open.c
@@ -877,24 +877,37 @@ EXPORT_SYMBOL(fd_install);
 
 long do_sys_open(int dfd, const char __user *filename, int flags, int mode)
 {
+	struct file *f;
 	char *tmp = getname(filename);
-	int fd = PTR_ERR(tmp);
+	int ret;
+	int fd;
 
-	if (!IS_ERR(tmp)) {
-		fd = get_unused_fd_flags(flags);
-		if (fd >= 0) {
-			struct file *f = do_filp_open(dfd, tmp, flags, mode, 0);
-			if (IS_ERR(f)) {
-				put_unused_fd(fd);
-				fd = PTR_ERR(f);
-			} else {
-				fsnotify_open(f->f_path.dentry);
-				fd_install(fd, f);
-			}
-		}
-		putname(tmp);
+	if (IS_ERR(tmp))
+		return PTR_ERR(tmp);
+
+	fd = get_unused_fd_flags(flags);
+	if (fd < 0) {
+		ret = fd;
+		goto out_name;
 	}
-	return fd;
+
+	f = do_filp_open(dfd, tmp, flags, mode, 0);
+	if (IS_ERR(f)) {
+		ret = PTR_ERR(f);
+		goto out_fd;
+	}
+
+	fsnotify_open(f->f_path.dentry);
+	fd_install(fd, f);
+
+	ret = fd;
+	goto out_name;
+
+out_fd:
+	put_unused_fd(fd);
+out_name:
+	putname(tmp);
+	return ret;
 }
 
 SYSCALL_DEFINE3(open, const char __user *, filename, int, flags, int, mode)
